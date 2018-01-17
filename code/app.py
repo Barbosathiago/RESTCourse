49af727c379f97f06557de779fd38535b32b6334
barbosathiago <thiago.barbosa@infouniron.com>
2018-01-16 22:58:24 -0400

    Section 3 example

--- code/app.py
+++ code/app.py
@@ -1,69 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-                'name': 'My Item',
-                'price': 15.99
-            }
-        ]
-    }
-]
+api = Api(app)
 
-# POST = used to receive data
-# GET = used to send data back only
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
 
-
-# POST /store data: {name:}
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
-
-# GET /store<string:name>
-@app.route('/store/<string:name>', methods=['GET'])
-def get_store(name):
-    # Iterate over get_stores
-    # If the store name matches, return it
-    # If none match, return an error
-    for store in stores:
-        if(store['name'] == name):
-            return jsonify(store)
-    return jsonify({'message': 'No stores found with that name'})
-
-# GET /store
-@app.route('/store', methods=['GET'])
-def get_stores():
-    return jsonify({'stores': stores})
-
-# POST /store/<string:name>/item {name:, price:}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if(store['name']==name):
-            new_item={
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message': 'store not found'})
-
-# GET /store/<string:name>/item
-@app.route('/store/<string:name>/item', methods=['GET'])
-def get_items_in_store(name):
-    for store in stores:
-        if(store['name'] == name):
-            return jsonify(store['items'])
-    return jsonify({'message': 'No items found for that store'})
+api.add_resource(Student, '/student/<string:name>') #http://localhost:5000/student/name
 
 app.run(port=5000)
---
+++ code/app_section_3.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+# POST = used to receive data
+# GET = used to send data back only
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+# GET /store<string:name>
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store(name):
+    # Iterate over get_stores
+    # If the store name matches, return it
+    # If none match, return an error
+    for store in stores:
+        if(store['name'] == name):
+            return jsonify(store)
+    return jsonify({'message': 'No stores found with that name'})
+
+# GET /store
+@app.route('/store', methods=['GET'])
+def get_stores():
+    return jsonify({'stores': stores})
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if(store['name']==name):
+            new_item={
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_items_in_store(name):
+    for store in stores:
+        if(store['name'] == name):
+            return jsonify(store['items'])
+    return jsonify({'message': 'No items found for that store'})
+
+app.run(port=5000)
